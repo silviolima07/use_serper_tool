2423f4b5143cbb11b1d74393c5d8eb9d120a7ca0
Silvio Lima <silviolima07@gmail.com>
2025-05-21 14:30:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if option == 'Pesquisar':
     st.markdown(html_page_crewai, unsafe_allow_html=True)
     
 
-    llm = MyLLM.QWEN
+    llm = MyLLM.DEEPSEEK
     guia_turistico = criar_agente_guia_turistico(llm)  # Passando o provider para o agente
     recomendar = criar_task_recomendar(guia_turistico)  # Passando o provider para a task
     
