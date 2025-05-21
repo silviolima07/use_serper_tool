62b3b14c3bc94b2f5335bf4e99dc90dfd06c257f
Silvio Lima <silviolima07@gmail.com>
2025-05-21 14:45:54 -0300

    Update MyLLM.py

--- MyLLM.py
+++ MyLLM.py
@@ -9,4 +9,4 @@ class MyLLM():
     LLAMA3_70B    = LLM(model='llama3-70b-8192')
     QWEN    = LLM(model='groq/qwen-qwq-32b')
     GROQ_MIXTRAL    = LLM(model='groq/mixtral-8x7b-32768')
-    GROQ_LLAMA   = LLM(model='groq/llama-4-scout-17b-16e-instruct')
+    GROQ_LLAMA   = LLM(model='groq/llama-3.3-70b-versatile')
